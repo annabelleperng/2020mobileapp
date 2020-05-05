a5f2f2d29047a5bd061e9e626b65db8565410e56
Damini Kaushik <daminik@alphonse.lan>
2020-05-05 16:48:35 -0700

    page changing

---
+++ ProductivityApp/Details.js
@@ -0,0 +1,29 @@
+// import * as React from 'react';
+import React, { Component } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Slider,
+  Dimensions,
+  Image
+} from "react-native";
+
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createStackNavigator } from '@react-navigation/stack';
+// import './App.js';
+
+export default class Details extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>YOURE A BITCHASS HOE!</Text>
+      </View>
+    );
+  }
+}
+// export default Details;
