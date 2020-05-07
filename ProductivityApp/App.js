b7ac50e00be74d91d4749272e3440d7149db72b2
Annabelle <annabelleperng@uchicago.edu>
2020-05-06 22:54:51 -0700

    Added necessary packages

--- ProductivityApp/App.js
+++ ProductivityApp/App.js
@@ -15,91 +15,36 @@ import {
   TouchableOpacity,
   Dimensions,
 } from "react-native";
-const screen = Dimensions.get("window");
+import Feedback from "./Feedback";
+// import Timer from "./Timer";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-const formatNumber = (number) => `0${number}`.slice(-2);
-const getRemaining = (time) => {
-  const mins = Math.floor(time / 60);
-  const secs = time - mins * 60;
-  return { mins: formatNumber(mins), secs: formatNumber(secs) };
-};
+const screen = Dimensions.get("window");
+const Stack = createStackNavigator();
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate("Feedback")}
+      />
+    </View>
+  );
+}
 
 export default function App() {
-  const [remainingSecs, setRemainingSecs] = useState(10);
-  const [isActive, setIsActive] = useState(false);
-  const { mins, secs } = getRemaining(remainingSecs);
-
-  const toggle = useCallback(() => {
-    setIsActive(!isActive);
-  });
-
-  useEffect(() => {
-    let interval = null;
-    if (isActive && remainingSecs > 0) {
-      interval = setInterval(() => {
-        setRemainingSecs((remainingSecs) => remainingSecs - 1);
-      }, 1000);
-    } else if (isActive) {
-      //   setRemainingSecs(0);
-      clearInterval(interval);
-    } else if (!isActive && remainingSecs !== 0) {
-      clearInterval(interval);
-    }
-    return () => clearInterval(interval);
-  }, [isActive, remainingSecs]);
   return (
     <View style={{ flex: 1 }}>
-      <View style={{ flex: 1.5, backgroundColor: "#222" }}>
-        {/* <View style={styles.container}>
-          <TouchableOpacity>
-            <Image
-              style={styles.tinyLogo}
-              source={require("./assets/offbutton.png")}
-            />
-          </TouchableOpacity>
-          <Text style={styles.fullTimeText}>Timer for 00:00:10</Text>
-        </View> */}
-
-        <View style={{ flexDirection: "row", marginTop: 50 }}>
-          <Text style={styles.fullTimeText}>Timer for 00:00:10</Text>
-          <TouchableOpacity>
-            <Image
-              style={styles.tinyLogo}
-              source={require("./assets/offbutton.png")}
-            />
-          </TouchableOpacity>
-        </View>
-      </View>
-      <View style={{ flex: 4, backgroundColor: "skyblue" }}>
-        <View style={styles.container}>
-          <StatusBar barStyle="light-content" />
-          <Text style={styles.timerText}>{`${mins}:${secs}`}</Text>
-        </View>
-      </View>
-      <View style={{ flex: 4, backgroundColor: "steelblue" }}>
-        <View style={styles.container}>
-          <TouchableOpacity onPress={toggle} style={styles.button}>
-            <Text style={styles.buttonText}>
-              {isActive ? (
-                <Text style={styles.unfocused}>I'M NOT FOCUSED</Text>
-              ) : (
-                <Text style={styles.focused}>FOCUS</Text>
-              )}
-            </Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-      <View style={{ flex: 2, backgroundColor: "#222" }}>
-        <View style={styles.containerChopped}>
-          <Text style={styles.red}>
-            This is your personal motivational quote if you entered one. Or
-            we'll just pick from ours.
-          </Text>
-          {/* <View style={{ flex: 0.5 }}>
-            <Image source={require("./assets/icon.png")} />
-          </View> */}
-        </View>
-      </View>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Timer">
+          {/* <Stack.Screen name="Home" component={HomeScreen} /> */}
+          <Stack.Screen name="Feedback" component={Feedback} />
+          {/* <Stack.Screen name="Timer" component={Timer} /> */}
+        </Stack.Navigator>
+      </NavigationContainer>
     </View>
   );
 }
