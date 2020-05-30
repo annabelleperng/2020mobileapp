0d37f0fd6f7bf68e4e9797170fde4cc018c942b0
Helen Yang <helentyang@berkeley.edu>
2020-05-29 18:45:46 -0700

    working on rewards page

--- ProductivityApp/RewardUtils.js
+++ ProductivityApp/RewardUtils.js
@@ -1,82 +1,111 @@
+import DateTime from "luxon/src/datetime.js";
+import Interval from "luxon/src/interval.js";
 import React from "react";
 import { StyleSheet, Text, View, Dimensions, Image } from "react-native";
-
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
   }
 
-  getWater() {
+  updateStreak = async () => {
+    localZone = this.props.route.params.selectedValues;
+    localTime = DateTime.local().setZone(localZone);
+    localMidnight = DateTime.fromObject({
+      year: localTime.year,
+      month: localTime.month,
+      day: localTime.day,
+      hour: 0,
+      minute: 0,
+      second: 0,
+      zone: localZone,
+    });
+    localPrevMidnight = localMidnight.minus({ days: 1 });
+    prevDay = Interval.fromDateTimes(localPrevMidnight, localMidnight);
+    latestSprintDay = DateTime.fromISO(
+      await SecureStore.getItemAsync("latest_sprint")
+    );
+    streakLength = Number.parseInt(
+      await SecureStore.getItemAsync("streak_length")
+    );
+    if (prevDay.contains(latestSprintDay)) {
+      streakLength += 1;
+    } else if (prevDay.isAfter(latestSprintDay)) {
+      streakLength = 0;
+    }
+    await SecureStore.setItemAsync("streak_length", "" + streakLength);
+  };
+
+  getWater = async () => {
     count = await SecureStore.getItemAsync("inventory_water");
     return count;
-  }
+  };
 
-  useWater(count) {
+  useWater = async (count) => {
     prevCount = await SecureStore.getItemAsync("inventory_water");
     newCount = Number.parseInt(prevCount) - count;
-    await SecureStore.setItemAsync("inventory_water", '' + newCount);
+    await SecureStore.setItemAsync("inventory_water", "" + newCount);
     return newCount;
-  }
+  };
 
-  earnWater(mins, streak) {
+  earnWater = async (mins, streak) => {
     prevCount = await SecureStore.getItemAsync("inventory_water");
     if (streak >= 3) {
-        added = mins * (1 + streak / 10)
+      added = mins * (1 + streak / 10);
     }
     newCount = Number.parseInt(prevCount) + added;
-    await SecureStore.setItemAsync("inventory_water", '' + newCount);
+    await SecureStore.setItemAsync("inventory_water", "" + newCount);
     return newCount;
-  }
+  };
 
-  getBees() {
+  getBees = async () => {
     count = await SecureStore.getItemAsync("inventory_bees");
     return count;
-  }
+  };
 
-  useBees(count) {
+  useBees = async (count) => {
     prevCount = await SecureStore.getItemAsync("inventory_bees");
     newCount = Number.parseInt(prevCount) - count;
-    await SecureStore.setItemAsync("inventory_bees", '' + newCount);
+    await SecureStore.setItemAsync("inventory_bees", "" + newCount);
     return newCount;
-  }
+  };
 
-  earnBees(mins, streak) {
+  earnBees = async (mins, streak) => {
     prevCount = await SecureStore.getItemAsync("inventory_bees");
     if (streak >= 3) {
-        added = (mins * (1 + streak / 10)) % 30
+      added = (mins * (1 + streak / 10)) % 30;
     }
     newCount = Number.parseInt(prevCount) + added;
-    await SecureStore.setItemAsync("inventory_bees", '' + newCount);
+    await SecureStore.setItemAsync("inventory_bees", "" + newCount);
     return newCount;
-  }
+  };
 
-  getGold() {
+  getGold = async () => {
     count = await SecureStore.getItemAsync("inventory_gold");
     return count;
-  }
+  };
 
-  useGold(count) {
+  useGold = async (count) => {
     prevCount = await SecureStore.getItemAsync("inventory_gold");
     newCount = Number.parseInt(prevCount) - count;
-    await SecureStore.setItemAsync("inventory_gold", '' + newCount);
+    await SecureStore.setItemAsync("inventory_gold", "" + newCount);
     return newCount;
-  }
+  };
 
-  earnGold(mins) {
+  earnGold = async (mins) => {
     prevCount = await SecureStore.getItemAsync("inventory_gold");
     if (streak >= 3) {
-        added = (mins * (1 + streak / 10)) * 5
+      added = mins * (1 + streak / 10) * 5;
     }
     newCount = Number.parseInt(prevCount) + added;
-    await SecureStore.setItemAsync("inventory_gold", '' + newCount);
+    await SecureStore.setItemAsync("inventory_gold", "" + newCount);
     return newCount;
-  }
+  };
 
-  obtainSeed(rarity, event) {
+  obtainSeed = async (rarity, event) => {
     prevStr = await SecureStore.getItemAsync("inventory_seeds");
     newStr = prevStr + "%" + rarity + event;
-    await SecureStore.setItemAsync("inventory_seeds", '' + newStr);
+    await SecureStore.setItemAsync("inventory_seeds", "" + newStr);
     return newStr;
-  }  
+  };
 }
